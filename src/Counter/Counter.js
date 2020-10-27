c61acb6cee3d7c8554d460bb440d43e5a9829931
Ben Hernandez <benjammin234@gmail.com>
2020-10-26 19:41:52 -0700

    Fleshing out wireframe

---
+++ src/Counter/Counter.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Counter.css";
+
+function Counter() {
+  return (
+    <div className="Counter">
+      <p>
+        Number of families helped so far: <strong>100</strong>
+      </p>
+    </div>
+  );
+}
+
+export default Counter;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,42 @@
 import React from "react";
+import Counter from "../Counter/Counter";
+import "./Home.css";
 
 function Home() {
   return (
-    <div>
-      <div className="logo">
-        <h1>Crate Full</h1>
+    <div className="Home">
+      <div className="img hero">
+        <h1 className="hero-logo">Crate Full</h1>
+      </div>
+
+      <div className="summary">
+        <div className="wrap">
+          <h2>What/Why is Crate Full?</h2>
+          <p>
+            Crate Full is a non-profit organization that provides food for those
+            in need. Food is homemade and/or homegrown and hand delivered to
+            families.
+          </p>
+        </div>
+
+        <div className="img fake-food"></div>
+      </div>
+
+      <div className="who-for">
+        <h2>Who is Crate Full For?</h2>
+        <p>Crate Full was created to benefit those who are without food.</p>
+        <button type="button">Who?</button>
+      </div>
+
+      <div className="counter">
+        <Counter />
+      </div>
+
+      <div className="how-help">
+        <h2>How Can You Help?</h2>
+        <p>We always need more food. We always need more hands.</p>
+        <button type="button">Donate</button>
+        <button type="button">Volunteer</button>
       </div>
     </div>
   );
