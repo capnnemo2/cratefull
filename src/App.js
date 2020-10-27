f8f27bf96204725a7e59d82464313e820ecb09a8
Ben Hernandez <benjammin234@gmail.com>
2020-10-26 18:25:33 -0700

    Creates Home and About

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Nav from "./Nav/Nav";
+import Home from "./Home/Home";
+import About from "./About/About";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
 }
-
-export default App;
